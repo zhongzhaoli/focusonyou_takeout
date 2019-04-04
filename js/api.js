ce0ce2fed25c8614920285fdd0efc4fd68373454
Custer <525966315@qq.com>
2019-04-05 00:01:22 +0800

    所有api 页面

--- js/api.js
+++ js/api.js
@@ -2,5 +2,9 @@ const api = {
     get_data: () => {
         let url = "https://api.yuntunwj.com/focusonyou/public/takeout";
         return ajax(url);
+    },
+    get_detail: (id) => {
+        let url = "https://api.yuntunwj.com/focusonyou/public/takeout/" + id;
+        return ajax(url);
     }
 };
